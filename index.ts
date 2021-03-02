9f5b728b66c93f172daa54c081d1c6a30e58bf3a
7dev7urandom <micah2004h@icloud.com>
2021-03-02 15:08:50 +0700

    done

--- index.ts
+++ index.ts
@@ -1,26 +1,6 @@
 const jsonReq = new XMLHttpRequest();
 jsonReq.open("GET", 'data.json', false);
 jsonReq.send();
-const dev = [
-    {
-        text: "Choice 1",
-        choices: [
-            ["Be awesome", 1],
-            ["Destroy the world", 2]
-        ],
-        isEnd: false
-    },
-    {
-        text: "Be awesome",
-        choices: [],
-        isEnd: true
-    },
-    {
-        text: "Destroy the world",
-        choices: [],
-        isEnd: true
-    }
-];
 const json = JSON.parse(jsonReq.responseText);
 
 var textArea;
@@ -33,21 +13,14 @@ window.addEventListener('load', () => {
     history.replaceState({ page: 0 }, '');
 });
 
-window.onpopstate = function({ state }) {
-    clickChoice(state.page, false);
-}
-
+window.onpopstate = ({ state }) => clickChoice(state.page, false);
 
 function clickChoice(x: number, historyMod = true) {
     textArea.innerHTML = json[x].text;
-    if(historyMod) {
-        history.pushState({ page: x }, json[x].text, '#');
+    if(historyMod) history.pushState({ page: x }, json[x].text);
+    if(json[x].isEnd) choicesList.innerHTML = `<button onclick="window.location.reload(true)">Reload</button>`;
+    else {
+        const choicesText = (json[x].choices as Array<[string, number]>).map(x => `<li><button onclick="clickChoice(${x[1]}, true)">${x[0]}</button></li>`).join('');
+        choicesList.innerHTML = choicesText;
     }
-    if(json[x].isEnd) {
-        choicesList.innerHTML = `<button onclick="window.location.reload(true)">Reload</button>`;
-        return;
-    }
-    const choicesText = (json[x].choices as Array<[string, number]>).map(x => `<li><button onclick="clickChoice(${x[1]}, true)">${x[0]}</button></li>`).join('');
-    choicesList.innerHTML = choicesText;
-
 }
